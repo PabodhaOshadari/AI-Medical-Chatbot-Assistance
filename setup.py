30bb335436da748e506fdf7e2a2ed6dd8b35974c
Oshadari <rasanikonara@gmail.com>
2025-07-08 12:05:51 +0530

    chatbot folder structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'AI Chatbot Project',
+    version= '0.0.0',
+    author= 'Pabodha Oshadari',
+    author_email= 'rasanikonara@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
